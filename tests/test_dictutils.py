1749151cd0382856cbdf85394d528d8968285c2a
Stefan Batalka <steba92@hotmail.com>
2021-04-01 01:23:45 +0200

    Reuse function implementations from sprig
    Because the next function to I plan to port is larger and I do not feel like copying
    it. Additionally this should make it easier eventually merging the repos.

--- tests/test_dictutils.py
+++ tests/test_dictutils.py
@@ -1,28 +1,7 @@
-from typing import Dict, TypeVar
-
 import pytest
 
 import sprigs
-
-
-T = TypeVar("T")
-V = TypeVar("V")
-
-
-# Reference implementation
-# Copied from sprig.dictutils
-def invert(mapping: Dict[T, V]) -> Dict[V, T]:
-    """Invert dictionary
-
-    Trivial function but good for two reasons:
-    * it gives the operation a name, and
-    * it catches an edge case that is easily forgotten.
-    """
-    result: Dict[V, T] = {v: k for (k, v) in mapping.items()}
-    if len(result) != len(mapping):
-        raise ValueError("Duplicate values in mapping")
-    return result
-
+from sprig import dictutils
 
 GOOD = [
     ({}, {}),
@@ -37,7 +16,7 @@ BAD = [
 
 
 @pytest.mark.parametrize("example", [vs[0] for vs in GOOD + BAD])
-@pytest.mark.parametrize("func", [sprigs.invert, invert,], ids=["rs", "py"])
+@pytest.mark.parametrize("func", [sprigs.invert, dictutils.invert], ids=["rs", "py"])
 def test_invert_does_not_mutate_imput(func, example):
     expected = example.copy()
     try:
@@ -48,14 +27,14 @@ def test_invert_does_not_mutate_imput(func, example):
 
 
 @pytest.mark.parametrize("example, expected", GOOD)
-@pytest.mark.parametrize("func", [sprigs.invert, invert], ids=["rs", "py"])
+@pytest.mark.parametrize("func", [sprigs.invert, dictutils.invert], ids=["rs", "py"])
 def test_returns_expected(benchmark, func, example, expected):
     actual = benchmark(func, example)
     assert actual == expected
 
 
 @pytest.mark.parametrize("before, cls", BAD)
-@pytest.mark.parametrize("func", [sprigs.invert, invert], ids=["rs", "py"])
+@pytest.mark.parametrize("func", [sprigs.invert, dictutils.invert], ids=["rs", "py"])
 def test_raises_expected(func, before, cls):
     with pytest.raises(cls):
         print(func(before))
